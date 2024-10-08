3317e8dc2f929d60d2f9a8fabe189316edce5dd1
johny02 <phanqu6ctoan@gmail.com>
2024-10-08 16:03:22 +0700

    feat(loaders): Implement Sentence Split

---
+++ core/loaders/base.py
@@ -0,0 +1,17 @@
+class BaseLoader:
+    """
+    Base class for all loaders.
+    """
+    def __init__(self):
+        self.chunk_size = 512
+        self.chunk_overlap = 100
+
+    def load(self, file):
+        raise NotImplementedError()
+
+    def split(self, data):
+        raise NotImplementedError()
+
+    def fit(self, file):
+        documents = self.load(file)
+        return self.split(documents)
