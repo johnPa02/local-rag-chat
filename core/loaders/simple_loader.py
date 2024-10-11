645f495a71674563bbcda13d7dde7de80d964bce
johny02 <phanqu6ctoan@gmail.com>
2024-10-11 18:25:21 +0700

    feat(loader): add text cleaning functionality to load method

--- core/loaders/simple_loader.py
+++ core/loaders/simple_loader.py
@@ -2,6 +2,8 @@ from llama_index.core.node_parser import SentenceSplitter, SemanticSplitterNodeP
 from llama_index.core import Document
 from core.loaders.base import BaseLoader
 import fitz
+import re
+
 
 class SimpleLoader(BaseLoader):
     """
@@ -12,12 +14,32 @@ class SimpleLoader(BaseLoader):
         super().__init__()
         self.embed_model = embed_model
 
+    @staticmethod
+    def _filter_text(text):
+        """
+        Filter text to remove unnecessary whitespaces and line breaks.
+        """
+        # Remove leading and trailing whitespaces
+        text = re.sub(r"\s+\n\s+", "\n", text)
+        # Remove spaces before line breaks
+        text = re.sub(r"\s+\n", "\n", text)
+        # Remove spaces after line breaks
+        text = re.sub(r"\n\s+", "\n", text)
+        # Remove line breaks which are not at the end of a sentence
+        text = re.sub(r"(?<![.!?])\n", " ", text)
+        # Limit the number of consecutive line breaks to 2
+        text = re.sub(r"\n{3,}", "\n\n", text)
+        return text
+
     def load(self, file):
         reader = fitz.open(file)
-        text = ""
+        text = []
         for page_num in range(reader.page_count):
             page = reader.load_page(page_num)
-            text += page.get_text()
+            page_text = page.get_text()
+            page_text = self._filter_text(page_text)
+            text.append(page_text)
+        text = " ".join(text)
         return [Document(text=text)]
 
     def split(self, documents):
