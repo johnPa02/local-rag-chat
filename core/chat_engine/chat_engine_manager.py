a825e0a929f3ec4f04bcc23c2473c69089ce20a7
johny02 <phanqu6ctoan@gmail.com>
2024-10-25 18:25:29 +0700

    feat(chat_engine): add condense plus context chat-engine

--- core/chat_engine/chat_engine_manager.py
+++ core/chat_engine/chat_engine_manager.py
@@ -1,16 +1,28 @@
-from llama_index.core.chat_engine import SimpleChatEngine
+from llama_index.core.chat_engine import SimpleChatEngine, CondensePlusContextChatEngine, CondenseQuestionChatEngine
 from llama_index.core.memory import ChatMemoryBuffer
 
 
 class ChatEngineManager:
-    def __init__(self, llm, query_engine, chat_mode='simple', memory_limit=3900):
-        self.query_engine = query_engine
+    def __init__(
+            self,
+            llm,
+            retriever,
+            chat_mode='condense_plus_context',
+            memory_limit=3900
+    ):
+        self.retriever = retriever
         self.llm = llm
         self.chat_mode = chat_mode
-        self.memory =  ChatMemoryBuffer(tokens_limit=memory_limit)
+        self.memory = ChatMemoryBuffer(tokens_limit=memory_limit)
 
     def get_engine(self):
         if self.chat_mode == 'simple':
             return SimpleChatEngine.from_defaults(llm=self.llm)
+        elif self.chat_mode == 'condense_plus_context':
+            return CondensePlusContextChatEngine.from_defaults(
+                self.retriever,
+                llm=self.llm,
+                memory=self.memory
+            )
         else:
             raise ValueError(f"Unsupported chat mode: {self.chat_mode}")
