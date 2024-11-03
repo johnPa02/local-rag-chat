6cb0b810c07c449f264f8d31b1691f92131f6676
johny02 <phanqu6ctoan@gmail.com>
2024-11-03 16:27:55 +0700

    feat(llms): pull Ollama model if not existed

--- core/llms/ollama.py
+++ core/llms/ollama.py
@@ -1,6 +1,8 @@
 from core.llms.base import BaseLLMModel
 from llama_index.llms.ollama import Ollama
 import requests
+from settings import OLLAMA_BASE_URL
+
 
 class OllamaModel(BaseLLMModel):
     def __init__(self, model: str):
@@ -8,25 +10,28 @@ class OllamaModel(BaseLLMModel):
         super().__init__()
 
     def load_llm(self):
+        if not self.model_exists():
+            self.pull_model()
+
         self.llm = Ollama(
             model=self.model,
-            base_url="http://ollama_server:11434",
-            request_timeout=60.0
+            base_url=OLLAMA_BASE_URL,
+            request_timeout=120
         )
+
+    def model_exists(self):
+        data = requests.get(f"{OLLAMA_BASE_URL}/api/tags").json()
+        return any(item['model']==self.model for item in data['models'])
 
     def predict(self, query):
         response = self.llm.complete(query)
         return response
 
-    def list_models(self):
-        return requests.get("http://ollama_server:11434/api/tags").json()
-
     def pull_model(self):
         payload = {
             "name": self.model
         }
-        return requests.post("http://ollama_server:11434/api/pull", json=payload).json()
+        return requests.post(f"{OLLAMA_BASE_URL}/api/pull", json=payload)
 
-    def check_model_exists(self):
-        pass
+
 
